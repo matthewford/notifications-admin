6c4ab5884e7584df0fedd9cf8b8b1fe338579e0e
Aleksey Zolotov <myxrome@gmail.com>
2021-03-01 18:12:43 +0400

    added sentry SDK integration.

--- application.py
+++ application.py
@@ -1,5 +1,12 @@
+import sentry_sdk
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 from app import create_app
+
+sentry_sdk.init(
+    integrations=[FlaskIntegration()],
+    traces_sample_rate=1.0
+)
 
 application = Flask('app')
 
