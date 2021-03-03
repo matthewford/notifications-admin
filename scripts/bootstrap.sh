600d252b68b0a10984c74c8aea644abb33b125f6
Dave J <dave.jones@bitzesty.com>
2021-03-03 09:56:47 +0000

    🔥 Remove superfluous display_results function
    This commit removes a `display_result` function that isn't used as part
    of the `scripts/bootstrap.sh` script. This function appears to be
    intended for displaying the results of a test-suite run, though the
    function is already defined in `scripts/run_tests.sh`, hence it's
    needlessly duplicated in our bootstrap script.
    
    This commit removes a `display_result` function that isn't used as part
    of the `scripts/bootstrap.sh` script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -17,19 +17,6 @@ export WERKZEUG_DEBUG_PIN=off
 "> environment.sh
 fi
 
-function display_result {
-  RESULT=$1
-  EXIT_STATUS=$2
-  TEST=$3
-
-  if [ $RESULT -ne 0 ]; then
-    echo -e "\033[31m$TEST failed\033[0m"
-    exit $EXIT_STATUS
-  else
-    echo -e "\033[32m$TEST passed\033[0m"
-  fi
-}
-
 if [ ! $VIRTUAL_ENV ]; then
   virtualenv -p python3 ./venv
   . ./venv/bin/activate
